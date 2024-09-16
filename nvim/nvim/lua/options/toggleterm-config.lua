bf0de8bbbbe7812226539b16c389ae5ff6808c1a
justyouraveragehasan <hasanali.soda@gmail.com>
2024-09-16 23:44:19 +0100

    checking for anything not committed

--- nvim/nvim/lua/options/toggleterm-config.lua
+++ nvim/nvim/lua/options/toggleterm-config.lua
@@ -1,6 +1,6 @@
 require("toggleterm").setup {
     size = 15,
-    open_mapping = [[<leader>t]],
+    open_mapping = [[<leader>T]],
     hide_numbers = true,
     shade_filetypes = {},
     shade_terminals = true,
@@ -11,3 +11,5 @@ require("toggleterm").setup {
 }
 
 
+vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n><C-w>w', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-n>', '<C-\\><C-n><C-w>w', { noremap = true, silent = true })
